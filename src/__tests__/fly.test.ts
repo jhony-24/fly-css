f3f3cb0348c3fed6a8fff9ef75e98291c038b28c
Jhony Vega <jhonyvegacuya24@gmail.com>
2021-02-16 18:40:28 -0500

    tests(Fly.test): Testing independent library properties

--- src/__tests__/fly.test.ts
+++ src/__tests__/fly.test.ts
@@ -12,18 +12,32 @@ describe("Fly-jss", () => {
         color: "blue",
         fontSize: "2em",
       },
+      floating: ({ background }) => ({
+        background,
+      }),
     });
   });
 
-  it("should empty classes", () => {
-    expect(styles.props()).toBe("");
-  });
+  describe('Usage of "props"', () => {
+    it("should empty classes", () => {
+      expect(styles.props()).toBe("");
+    });
 
-  it("should have three class names", () => {
-    expect(styles.props("title", "button").split(" ")).toHaveLength(3);
-  });
+    it("should have three class names", () => {
+      expect(styles.props("title", "button").split(" ")).toHaveLength(3);
+    });
 
-  it("should not have class names if receive an false argument", () => {
-    expect(styles.props(false, null, undefined)).toBe("");
+    it("should not have class names if receive an false argument", () => {
+      expect(styles.props(false, null, undefined)).toBe("");
+    });
+
+    describe('Use of "dynamics"', () => {
+      it("should get correct class names", () => {
+        const getClassNames = styles.dynamic.floating({
+          background: " blue",
+        });
+        expect(getClassNames.split(" ")).toHaveLength(1);
+      });
+    });
   });
 });
